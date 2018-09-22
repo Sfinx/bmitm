08cbf9c66e94f84a46bfbaa1b765b221681d4047
Rus V. Brushkoff <rus@sfinx.in>
2018-09-22 17:00:27 +0300

    Add TODO

--- mitm.h
+++ mitm.h
@@ -101,6 +101,7 @@ class mitm_conn_t {
      tcp->disconnect();
      std::cerr << "conn N" << cid << " app: disconnected\n";
    }
+   // TODO: move tcp/proxy->isPending() branches to own separate threads, will improve perfomance
    bool mitm_run() {
      if (tcp->isPending(ost::Socket::pendingInput, PENDING_SLEEP_MS)) {
        ssize_t sz = tcp->readData(buf, sizeof(buf));
@@ -138,7 +139,7 @@ class autodeleted_thread {
 public:
   explicit autodeleted_thread(std::thread t_): t(std::move(t_)) {
     if (!t.joinable())
-      throw std::logic_error("No thread");
+      throw std::logic_error("No thread !");
   }
   ~autodeleted_thread() { t.detach(); }
   autodeleted_thread(autodeleted_thread&)= delete;
@@ -146,21 +147,21 @@ public:
 };
 
 class mitm_t : public ost::TCPSocket, public ost::Thread {
-  uint cid;
   data_cb_t app_tx, app_rx;
-  bool app_tx_cb(uint cid_, std::string &d) { return app_tx(cid_, d); }
-  bool app_rx_cb(uint cid_, std::string &d) { return app_rx(cid_, d); }
+  bool app_tx_cb(uint cid, std::string &d) { return app_tx(cid, d); }
+  bool app_rx_cb(uint cid, std::string &d) { return app_rx(cid, d); }
  public:
   mitm_t(u16 p, data_cb_t app_tx_, data_cb_t app_rx_) : TCPSocket(ost::IPV4Address("0.0.0.0"), p),
-    cid(0), app_tx(app_tx_), app_rx(app_rx_) { }
+    app_tx(app_tx_), app_rx(app_rx_) { }
   void run() {
+    static uint cid;
     while(1) {
       if (isPendingConnection()) {
         auto process_mitm = [](const int cid_, mitm_t *m) {
           // TCPStream(TCPSocket &server, bool throwflag = true, timeout_t timeout = 0);
           ost::TCPStream *tcp = new ost::TCPStream(*m, true, PENDING_SLEEP_MS);
           mitm_conn_t nc(cid_, tcp, std::bind(&mitm_t::app_tx_cb, m, ph::_1, ph::_2),
-           std::bind(&mitm_t::app_rx_cb, m, ph::_1, ph::_2));
+            std::bind(&mitm_t::app_rx_cb, m, ph::_1, ph::_2));
           nc.run();
           delete tcp;
         };
